5151bc3a312e29188ef36a9bd54e0fff34d0ac58
Your Name <you@example.com>
2022-06-30 08:42:32 +0100

    completed 2

--- galacars/settings.py
+++ galacars/settings.py
@@ -169,7 +169,7 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'galaCar.24.43@gmail.com'
-EMAIL_HOST_PASSWORD = 'azerty1234//'
+EMAIL_HOST_PASSWORD = '*******'
 EMAIL_USE_TLS = True
 #EMAIL_USE_SSL = fail_silently
 
